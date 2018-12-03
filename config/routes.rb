2caf605c3b958443ed69586d78009f9eec996471
wgeorge1990 <williameverettgeorge@icloud.com>
2018-12-03 17:15:21 -0500

    migrated

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+    resources :categories
 
+    resources :users do
+      resources :saved_gems
+    end
+
+    resources :city_gems do
+      resources :comments
+      resources :likes
+    end
+    
 end
--- db/migrate/20181203192420_create_comments.rb
+++ db/migrate/20181203192420_create_comments.rb
@@ -4,7 +4,6 @@ class CreateComments < ActiveRecord::Migration[5.2]
       t.string :body
       t.integer :user_id
       t.integer :city_gem_id
-      t.integer :likes
       t.datetime "created_at", null: false
       t.datetime "updated_at", null: false
       t.timestamps
