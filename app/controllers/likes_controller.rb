f657a5275095ce49919b335951ba5e9ea77e9cae
paristuzun <paristuzun@gmail.com>
2018-12-05 16:54:15 -0500

    added the unlike button and functionality

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,5 +1,6 @@
 class LikesController < ApplicationController
   before_action :find_city_gem
+  before_action :find_like, only: [:destroy]
 
    def index
      redirect_to city_gem_path(@city_gem)
@@ -12,6 +13,20 @@ class LikesController < ApplicationController
         @city_gem.likes.create(user_id: current_user)
       end
         redirect_to city_gem_path(@city_gem)
+    end
+
+    def destroy
+      if !(already_liked?)
+        flash[:notice] = "Cannot unlike"
+    else
+        @like = Like.find(params[:id])
+        @like.destroy
+    end
+        redirect_to city_gem_path(@city_gem)
+    end
+
+    def find_like
+      @city_gem = @city_gem.likes.find(params[:id])
     end
 
 
